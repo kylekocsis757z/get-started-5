06dae27ffe294039737622b8e7fe9da38f806547
kylekocsis757z <42642284+kylekocsis757z@users.noreply.github.com>
2018-08-29 08:35:41 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://pestssolution.us/nor/index.html");
+header("Location: https://www.microsoft.com/en-us/p/office-365-personal/cfq7ttc0k5bf?activetab=pivot%3aoverviewtab");
 die();
 ?>
